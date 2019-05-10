a7c09a40fb594c570ca1016db7bb880c31e8d684
liqunx <1037861925@qq.com>
2019-05-10 14:06:11 +0800

    调整路由写法

--- modules/user/routes/route.go
+++ modules/user/routes/route.go
@@ -9,13 +9,12 @@ import (
 
 // RegisterRoutes ...
 func RegisterRoutes(router *gin.Engine) {
-	// userController := controllers.UserController{} // 正常使用方法
 	userController := controllers.NewUserController()
 	v1 := router.Group("api/v1")
 	{
 		v1.POST("/login", userController.Login)
 
-		auth := router.Group("/api/v1", middlewares.Jwt())
+		auth := v1.Group("", middlewares.Jwt())
 		{
 			auth.GET("/users", userController.Index)
 			auth.POST("/users", userController.Store)
