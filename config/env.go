ed47734ed5039af8951154b4c759a9a48aac1630
洪剑宏 <565983236@qq.com>
2019-05-20 11:57:42 +0800

    修复go test .env路径错误

--- config/env.go
+++ config/env.go
@@ -2,17 +2,27 @@ package config
 
 import (
 	"log"
-	"os"
+	"path/filepath"
+	"runtime"
 
 	"github.com/joho/godotenv"
 )
 
 // init 加载环境变量
 func init() {
-	if name := os.Getenv("APP_NAME"); name != "" {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error loading .env file!")
-		}
+	_, filename, _, _ := runtime.Caller(0)
+	// fmt.Println("Current test filename: " + filename)
+
+	dir, err := filepath.Abs(filepath.Dir(filename) + "/../")
+
+	if err != nil {
+		log.Fatal(err)
 	}
+	// fmt.Println(dir)
+
+	err = godotenv.Load(dir + "/.env")
+	if err != nil {
+		log.Fatal("Error loading .env file!")
+	}
+
 }
